50d3e9cf271561a7bd6d32df7b4fd58961ffdeeb
Sergi Reyner <sergi.reyner@gmail.com>
2017-03-15 20:59:31 +0000

    Introduced the constant +FALSE+
    Holds the AST node fot boolean false, since it's used all around.
    This change is in preparation for fixing booleans and the && and ||
    operators.

--- parser.lisp
+++ parser.lisp
@@ -2,6 +2,9 @@
 
 (in-package #:minilang)
 
+;; the node for boolean false, defined here because it's used all around
+(defconstant +false+ (list :type "bool" :value nil))
+
 (defclass parser ()
   ((input :accessor input :initarg :input)))
 
