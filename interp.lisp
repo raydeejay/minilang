fed5052cd107625a1986fd896aed3ddc1266e25e
Sergi Reyner <sergi.reyner@gmail.com>
2017-03-15 23:14:08 +0000

    Converted the interpreter to CPS style
    The rest of the functions have been adapted as well.

--- interp.lisp
+++ interp.lisp
@@ -3,52 +3,86 @@
 (in-package #:minilang)
 
 ;; interpreter
-(defun evaluate (exp env)
+(defun evaluate (exp env callback)
   (switch ((<- :type exp) :test 'equal)
     ("num"
-      (<- :value exp))
+     (funcall callback (<- :value exp)))
     ("str"
-      (<- :value exp))
+     (funcall callback (<- :value exp)))
     ("bool"
-      (<- :value exp))
+     (funcall callback (<- :value exp)))
     ("var"
-     (getv env (<- :value exp)))
+     (funcall callback (getv env (<- :value exp))))
     ("assign"
      (if (not (equal (<- :type (<- :left exp)) "var"))
          (error "Cannot assign to ~A" (<- :left exp))
-         (setv env
-               (<- :value (<- :left exp))
-               (evaluate (<- :right exp) env))))
+         (evaluate (<- :right exp)
+                   env
+                   (lambda (right)
+                     (setv env
+                           (<- :value (<- :left exp))
+                           right)
+                     (funcall callback env)))))
     ("binary"
-     (apply-op (<- :operator exp)
-               (evaluate (<- :left exp) env)
-               (evaluate (<- :right exp) env)))
+     (evaluate (<- :left exp)
+               env
+               (lambda (left)
+                 (evaluate (<- :right exp)
+                           env
+                           (lambda (right)
+                             (funcall callback (apply-op (<- :operator exp)
+                                                         left
+                                                         right)))))))
     ("lambda"
-     (make-lambda env exp))
+     (funcall callback (make-lambda env exp)))
     ("let"
-     (loop :for v :in (<- :vars exp)
-        :do (let ((scope (extend env)))
-              (def scope (<- :name v)
-                (if (<- :def v)
-                    (evaluate (<- :def v) env)
-                    nil))
-              (setf env scope)))
-     (evaluate (<- :body exp) env))
+     (labels ((loop% (env i)
+                 (if (< i (length (<- :vars exp)))
+                     (let ((v (nth i (<- :vars exp))))
+                       (if (<- :def v)
+                           (evaluate (<- :def v)
+                                     env
+                                     (lambda (value)
+                                       (let ((scope (extend env)))
+                                         (def scope (<- :name v) value)
+                                         (loop% scope (1+ i)))))
+                           (let ((scope (extend env)))
+                             (def scope (<- :name v) nil)
+                             (loop% scope (1+ i)))))
+                     (evaluate (<- :body exp) env callback))))
+       (loop% env 0)))
     ("if"
-     (if (evaluate (<- :cond exp) env)
-         (evaluate (<- :then exp) env)
-         (if (<- :else exp)
-             (evaluate (<- :else exp) env))))
+     (evaluate (<- :cond exp)
+               env
+               (lambda (cond)
+                 (if cond
+                     (evaluate (<- :then exp) env callback)
+                     (if (<- :else exp)
+                         (evaluate (<- :else exp) env callback)
+                         (funcall callback nil))))))
     ("prog"
-     (loop :with val := nil
-        :for subexp :in (<- :prog exp)
-        :do (setf val (evaluate subexp env))
-        :finally (return val)))
+     (labels ((loop% (last i)
+                 (if (< i (length (<- :prog exp)))
+                     (evaluate (nth i (<- :prog exp))
+                               env
+                               (lambda (val)
+                                 (loop% val (1+ i))))
+                     (funcall callback last))))
+       (loop% nil 0)))
     ("call"
-     (let ((func (evaluate (<- :func exp) env)))
-       (apply func
-              (mapcar (lambda (arg) (evaluate arg env))
-                      (<- :args exp)))))
+     (evaluate (<- :func exp)
+               env
+               (lambda (func)
+                 (labels ((loop% (args i)
+                             (if (< i (length (<- :args exp)))
+                                 (evaluate (nth i (<- :args exp))
+                                           env
+                                           (lambda (arg)
+                                             (loop% (append args
+                                                            (list arg))
+                                                (1+ i))))
+                                 (apply func args))))
+                   (loop% (list callback) 0)))))
     (otherwise (error "I don't know how to evaluate ~A"
                       (<- :type exp)))))
 
@@ -79,23 +113,16 @@
 
 ;; I know... but I can't figure out how to do it properly right now
 (defun make-lambda (env exp)
-  (let ((lambda-env (if (<- :name exp) (extend env) env)))
-    (if (<- :name exp)
-        (def lambda-env (<- :name exp)
-          (lambda (&rest args)
-            (let ((params (<- :vars exp))
-                  (scope (extend lambda-env)))
-              (loop :for name :in params
-                 :for n :from 0
-                 :do (def scope name (nth n args))
-                 :finally (return (evaluate (<- :body exp)
-                                            scope))))))
-        (lambda (&rest args)
-            (let ((params (<- :vars exp))
-                  (scope (extend lambda-env)))
-              (loop :for name :in params
-                 :for n :from 0
-                 :do (def scope name (nth n args))
-                 :finally (return (evaluate (<- :body exp)
-                                            scope))))))))
+  (labels ((lambda% (callback &rest args)
+             (let ((params (<- :vars exp))
+                   (scope (extend env)))
+               (when (<- :name exp)
+                 (def scope (<- :name exp) #'lambda%))
+               (loop :for name :in params
+                  :for n :from 0
+                  :do (def scope name (nth n args))
+                  :finally (return (evaluate (<- :body exp)
+                                             scope
+                                             callback))))))
+    #'lambda%))
 
--- minilang.lisp
+++ minilang.lisp
@@ -20,9 +20,15 @@
 
 (defun base-env ()
   (let ((env (make-instance 'environment)))
-    (def env "print" (lambda (txt) (format t "~A" txt)))
-    (def env "println" (lambda (txt) (format t "~A~%" txt)))
-    (def env "quit" (lambda () (throw 'quit t)))
+    (def env "print" (lambda (callback txt)
+                       (format t "~A" txt)
+                       (funcall callback nil)))
+    (def env "println" (lambda (callback txt)
+                         (format t "~A~%" txt)
+                         (funcall callback nil)))
+    (def env "quit" (lambda (callback)
+                      (declare (ignore callback))
+                      (throw 'quit t)))
     env))
 
 (defun prelude ()
@@ -57,10 +63,14 @@ range = lambda(a, b)
                     else NIL;
 ")
 
+;; TODO: catch errors and don't crash the REPL
 (defun repl ()
   (catch 'quit
     (loop :with env := (base-env)
-       :initially (evaluate (parse (prelude)) env)
-       (format t "Welcome to the minilang REPL~%> ")
+       :initially (evaluate (parse (prelude)) env 'identity)
+       :initially (format t "Welcome to the minilang REPL~%> ")
        :doing (format t "~A~%> "
-                      (evaluate (parse (read-line)) env)))))
+                      (evaluate (parse (read-line))
+                                env
+                                (lambda (result)
+                                  (print result)))))))
