ea248dd953da5e95afee3b4e047c7546bcbfd749
Sergi Reyner <sergi.reyner@gmail.com>
2017-03-21 04:01:42 +0000

    Moved DISMISS to utils.lisp

--- utils.lisp
+++ utils.lisp
@@ -10,6 +10,10 @@
   (defun gensym% (&optional (name "") (prefix "α"))
     (format nil  "~A_~A~D" prefix name (incf gensym-counter))))
 
+(defun dismiss (&rest args)
+  "Takes any number of arguments and simply ignores them, doing absolutely nothing."
+  (declare (ignore args)))
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defpackage minilang-runtime
     (:documentation "Holds minilang runtime definitions")))
