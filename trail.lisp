4ddc5a8db9441277d31d88b062435335526c40f9
Sergi Reyner <sergi.reyner@gmail.com>
2017-03-28 13:33:01 +0100

    Added methods to nodes
    HAS-PARTIAL-SHAPE-P returns T if the node has a number of vertexes that
    will cause incomplete shapes (spurious vertices).
    
    PARTIAL-SHAPE returns the list of spurious vertices from the top of
    the VERTICES list.
    
    COMPLETE-SHAPES returns the list of vertices minus the spurious ones.
    
    Simplified forms, added comments, moved code around.

--- trail.lisp
+++ trail.lisp
@@ -8,7 +8,7 @@
 
 (defmethod add ((trail trail) node-class)
   ;; remove top node if it's empty
-  (when (emptyp (car (nodes trail)))
+  (when (emptyp (top trail))
     (pop (nodes trail)))
   (push (make-instance node-class
                        :color (color *turtle*)
@@ -18,7 +18,11 @@
 (defmethod top ((trail trail))
   (car (nodes trail)))
 
+(defparameter *trail* (list (make-instance 'lines-node))
+  "Holds a list of nodes to redraw each frame.")
 
+
+;; the base node
 (defclass node ()
   ((primitive :accessor primitive :initarg :primitive)
    (vertices  :accessor vertices  :initarg :vertices  :initform nil)
@@ -41,19 +45,38 @@
     (gl:color r g b))
   (gl:line-width (width node)))
 
-
+;; points (WIP)
 (defclass points-node (node)
   ((primitive :reader primitive :initform :points)))
 
 
+;; lines
 (defclass lines-node (node)
   ((primitive :reader primitive :initform :line-strip)))
 
+(defmethod has-partial-shape-p ((node lines-node))
+  (= 1 (length (vertices node))))
 
+(defmethod partial-shape ((node lines-node))
+  ;; stub
+  )
+
+(defmethod complete-shapes ((node lines-node))
+  (vertices node))
+
+
+;; triangles
 (defclass triangles-node (node)
   ((primitive :reader   primitive :initform :triangles)))
 
+(defmethod has-partial-shape-p ((node triangles-node))
+  (plusp (mod (length (vertices node)) 3)))
 
+(defmethod partial-shape ((node triangles-node))
+  (subseq (vertices node)
+          0
+          (mod (length (vertices node)) 3)))
 
-(defparameter *trail* (list (make-instance 'lines-node))
-  "Holds a list of nodes to redraw each frame.")
+(defmethod complete-shapes ((node triangles-node))
+  (subseq (vertices node)
+          (mod (length (vertices node)) 3)))
